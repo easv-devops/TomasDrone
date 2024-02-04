8481f5de3b6a7516b290b8aa32ac2ea879576ab4
Tomas1974 <114471994+Tomas1974@users.noreply.github.com>
2024-02-04 11:23:39 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+using NUnit.Framework;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+public class MyMath
+{
+
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+
+    public int Sub(int a, int b)
+    {
+        return a - b;
+    }
+    
+}
+
+
